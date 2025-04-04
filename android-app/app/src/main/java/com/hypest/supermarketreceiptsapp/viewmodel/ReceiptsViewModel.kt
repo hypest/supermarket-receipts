c8bac7fc75e101aa2ea237b76bee22499ba2da04
Stefanos Togoulidis <stefanostogoulidis@gmail.com>
2025-04-04 04:02:35 +0300

    More cleanup

--- android-app/app/src/main/java/com/hypest/supermarketreceiptsapp/viewmodel/ReceiptsViewModel.kt
+++ android-app/app/src/main/java/com/hypest/supermarketreceiptsapp/viewmodel/ReceiptsViewModel.kt
@@ -1,19 +1,12 @@
 package com.hypest.supermarketreceiptsapp.viewmodel
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
-import com.hypest.supermarketreceiptsapp.data.repository.ReceiptRepositoryImpl
 import com.hypest.supermarketreceiptsapp.domain.model.Receipt
 import com.hypest.supermarketreceiptsapp.domain.repository.ReceiptRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
-import io.github.jan.supabase.realtime.PostgresAction
-import io.github.jan.supabase.realtime.channel
-import io.github.jan.supabase.realtime.postgresChangeFlow
-import io.github.jan.supabase.realtime.realtime
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.flow.SharingStarted.Companion.WhileSubscribed
-import kotlinx.coroutines.launch // Import launch coroutine builder
 import javax.inject.Inject
 
 data class ReceiptsUiState(
@@ -30,38 +23,6 @@ class ReceiptsViewModel @Inject constructor(
     companion object {
         private const val TAG = "ReceiptsViewModel" // Add TAG definition
     }
-
-//    init {
-//        val channel2 = (receiptRepository as ReceiptRepositoryImpl).supabaseClient
-//            .realtime.channel("test")
-//        val dataFlow = channel2.postgresChangeFlow<PostgresAction>(schema = "public")
-//        dataFlow.onEach {
-//            when(it) {
-//                is PostgresAction.Delete -> {
-//                    Log.d(TAG, "Got delete")
-//                }
-//                is PostgresAction.Insert -> {
-//                    Log.d(TAG, "Got insert")
-//                }
-//                is PostgresAction.Select -> {
-//                    Log.d(TAG, "Got select")
-//                }
-//                is PostgresAction.Update -> {
-//                    Log.d(TAG, "Got update")
-//                }
-//            }
-//        }.launchIn(viewModelScope) // Use viewModelScope directly
-//
-//        // Launch subscribe in a coroutine
-//        viewModelScope.launch {
-//            try {
-//                channel2.subscribe()
-//                Log.d(TAG, "Subscribed to test channel")
-//            } catch (e: Exception) {
-//                Log.e(TAG, "Error subscribing to test channel", e)
-//            }
-//        }
-//    }
 
     // Convert the Flow<Result<List<Receipt>>> directly into StateFlow<ReceiptsUiState>
     val uiState: StateFlow<ReceiptsUiState> = receiptRepository.getReceipts()
